c2be222a63a5a18ea809128ec8f8f421a8bc34c4
zinzin68 <dnwls3958@naver.com>
2023-06-03 00:14:09 +0900

    set start

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
+import sys
+import os
+from PySide6 import QtUiTools, QtGui
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+class MainView(QMainWindow):    
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+
+    def setupUI(self):
+        global UI_set
+
+        UI_set = QtUiTools.QUiLoader().load(resource_path("WRP_Start.ui"))
+        self.setCentralWidget(UI_set)
+        self.setWindowTitle("UI TEST")
+        self.setWindowIcon(QtGui.QPixmap(resource_path("./images/word.png")))
+        self.setGeometry(100,50,300,200)
+        self.show()
+ 
+
+#파일 경로
+#pyinstaller로 원파일로 압축할때 경로 필요함
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = MainView()
+    #main.show()
+    sys.exit(app.exec())
+
+
 import openpyxl
+
 wb = openpyxl.load_workbook("C:/Users/dnwls/WRP/engword.xlsx")
 wb_sheet = wb.active
 
